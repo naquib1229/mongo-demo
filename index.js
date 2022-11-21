9f61b76830a1d1f86a9e9f3c46e1c5c5aee9fdba
naquib1229 <naquib.alam1229@gmail.com>
2022-11-21 12:38:54 +0530

    Schemas

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-//Connecting to Mongodb
+//Schemas
+//We use Schema to define the shape of documents within a collection in a MongoDB
+//Schema is a specific to mongoose not part of MongoDB
+//Schema Types- String, Number, Date, Bufffer, Boolean, ObjectID and Arrays
+
  const mongoose = require('mongoose');
 
- mongoose.connect('mongodb://localhost/playground')// connection string hard coded but in real world application it comes from configuration file
-    .then(() => console.log('Connected to MongoDB...')) // if playground database not exist created playground database and connect
+ mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to MongoDB...')) 
     .catch(err => console.error('Could not connect to MongoDB...', err));
+
+const CourseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {type: Date, default: Date.now},
+    isPublished: Boolean
+});
