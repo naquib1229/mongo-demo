0bec3ec99e6135d19add77bda3213e091ea5b66b
naquib1229 <naquib.alam1229@gmail.com>
2022-11-24 20:43:46 +0530

    Exercise 1

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
-//Pagination
+//Exercise 1
+const mongoose = require('mongoose');
 
- const mongoose = require('mongoose');
-
- mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log('Connected to MongoDB...')) 
-    .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose.connect('mongodb://localhost/mongo-exercises');
 
 const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: {type: Date, default: Date.now},
-    isPublished: Boolean
+  name: String,
+  author: String, 
+  tags: [ String ],
+  date: Date, 
+  isPublished: Boolean,
+  price: Number
 });
 
-const Course = mongoose.model('Course', courseSchema); //model return class
+const Course = mongoose.model('Course', courseSchema);
 
-// async function createCourse() {
-//     const course = new Course({
-//     name: 'Angular course',
-//     author: 'Mosh',
-//     tags: ['Angular', 'frontend'],
-//     isPublished: true
-// });
-
-// const result =  await course.save();
-// console.log(result);
-// }
-
-//createCourse();
-
-async function getCourse() {
-    const pageNumber = 2;
-    const pageSize =10;
-    
-    const courses = await Course
-        .find({author: 'Mosh', isPublished: true})
-        .skip((pageNumber -1)*pageSize)
-        .limit(pageSize)
-        .sort({name: 1})            
-        .count();
-    console.log(courses);
+async function getCourses() {
+  return await Course
+  .find({ isPublished: true, tags: 'backend' })
+  .sort({ name: 1 })
+  .select({ name: 1, author: 1 });
 }
 
-getCourse();
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();
