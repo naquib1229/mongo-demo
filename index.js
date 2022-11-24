a4a5536dc5c8a8b77281d903f36a35c7b553fcd6
naquib1229 <naquib.alam1229@gmail.com>
2022-11-24 21:15:33 +0530

    Exercise3

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-//Exercise 2
-//Get all the published frontend and backenc course, sort them by their price in a descending order,
-//Pick only their name and author and display them.
+//Exercise 3
+//Get all the published courses that are $15 or more. or have the word 'by' in their title
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/mongo-exercises');
@@ -19,7 +18,8 @@ const Course = mongoose.model('Course', courseSchema);
 async function getCourses() {
   return await Course
   .find({ isPublished: true }) 
-  .or([{tags: 'frontend'}, {tags: 'backend'}]) 
+  .or([{price: { $gte: 15 }}, 
+    { name: /.*by.*/i }]) 
   .sort('-price')                                           
   .select('name author price');                                   
 }
