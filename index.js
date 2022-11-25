48049cd557bd8ec7dd7fe9e0e62a85d95d510171
naquib1229 <naquib.alam1229@gmail.com>
2022-11-25 08:18:02 +0530

    Validation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-//Removing Documents
+//Validation
+//this validation implemented here is only meaningful in Mongoose and not MongoDB like SQL DB.
+//Joi vs mongoose validation, we should use both Joi insures in RESTapi that user doesn't entered wrong data, here mongoose ensures after that any flaw not entered by mistake.
 
 const mongoose = require('mongoose');
 
@@ -7,15 +9,36 @@ mongoose.connect('mongodb://localhost/playground')
    .catch(err => console.error('Could not connect to MongoDB...', err));
    
 const courseSchema = new mongoose.Schema({
-   name: String,
+   name: {type: String, required: true},
    author: String,
    tags: [String],
    date: {type: Date, default: Date.now},
-   isPublished: Boolean
+   isPublished: Boolean,
+   price: Number
 });
 
 const Course = mongoose.model('Course', courseSchema); //model return class
 
+async function createCourse() {
+   const course = new Course( {
+     //name: 'Angular course',
+     author: 'Mosh',
+     tags: ['angular', 'frontend'],
+     isPublished: true,
+     price: 15
+   })
+   try{
+      //await course.validate();
+      const result = await course.save();
+      console.log(result);  
+   }
+   catch(ex){
+      console.log(ex.message);
+   }
+   
+ }
+
+ 
 async function updateCourse(id){
     
     const  course = await Course.findByIdAndUpdate( id, {
@@ -34,6 +57,6 @@ async function removeCourse(id){
    console.log(course);
 }
 
-removeCourse('637b28910bf936cd8edbcccd');
+createCourse();
 
 
