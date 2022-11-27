7ff7a0150b4cf49b3bde693b9cf3eb002dab62c0
naquib1229 <naquib.alam1229@gmail.com>
2022-11-27 16:18:32 +0530

    Population IV

--- population.js
+++ population.js
@@ -47,7 +47,8 @@ async function createCourse(name, author) {
 async function listCourses() { 
   const courses = await Course
     .find()
-    .populate('author', 'name')
+    .populate('author', 'name -_id')
+    //.populate('category', 'name')
     .select('name author');
   console.log(courses);
 }
